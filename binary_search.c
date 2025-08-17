2d2876f1fa2832b242c8059d508a1f5d55c482a5
Jeon Jeongrim <jrjeon1115@gmail.com>
2025-08-17 15:16:39 +0900

    basic bin_search code and change name search=>linearch

---
+++ binary_search.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int bin_search(const int a[],int n, int key)
+{
+	int s = 0;
+	int e = n-1;
+	while (1)
+	{
+		int m = (s + e) / 2;
+		if (a[m] > key)
+		{
+			e = m-1;
+		}
+		else if(a[m] < key)
+		{
+			s = m+1;
+		}
+		else if (a[m]==key)
+		{
+			return m;
+		}
+		else if (s > e)
+		{
+			return -1;
+		}
+	}
+}
+
+int main(void)
+{
+	int nx, ky;
+
+	puts("binary search");
+	printf("number of elements: ");
+	scanf("%d", &nx);
+	int* x = calloc(nx, sizeof(int));
+	printf("오름차 순으로 입력하세요.\n");
+	printf("x[0]: ");
+	scanf("%d", &x[0]);
+	for (int i = 1; i < nx; i++)
+	{
+		do {
+			printf("x[%d]: ", i);
+			scanf("%d", &x[i]);
+		} while (x[i] < x[i - 1]);
+	}
+	printf("search value: ");
+	scanf("%d", &ky);
+	int idx = bin_search(x, nx, ky);
+	if (idx == -1)
+	{
+		puts("search is failed");
+	}
+	else
+	{
+		printf("%d is located at x[%d].", ky, idx);
+	}
+	free(x);
+
+	return 0;
+}
