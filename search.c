fb8148246ba50768c62dc270fc2d4bd45bc1bbb2
Jeon Jeongrim <jrjeon1115@gmail.com>
2025-08-17 13:48:16 +0900

    use while linear search

---
+++ search.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(const int a[], int n, int key)
+{
+	int i = 0;
+	while (1)
+	{
+		if (i == n)
+		{ 
+			return -1;
+		}
+		if (a[i] == key)
+		{
+			return i;
+		}
+		i++;
+	}
+}
+int main(void)
+{
+	int nx, ky;
+	puts("linear search");
+	printf("number of elemests: ");
+	scanf("%d", &nx);
+	int *x = calloc(nx, sizeof(int));
+	for (int i = 0; i < nx; i++)
+	{
+		printf("x[%d]: ", i);
+		scanf("%d", &x[i]);
+	}
+	printf("search value: ");
+	scanf("%d", &ky);
+	int idx = search(x, nx, ky);
+	if (idx == -1)
+	{
+		puts("search is failed");
+	}
+	else
+	{
+		printf("%d is located at x[%d].\n", ky, idx);
+	}
+	free(x);
+
+	return 0;
+}
